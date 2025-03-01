b1d16d7a43b5e02cfa342ce20c912fcb7bfec5dc
chi <chi.adiukwu@outlook.com>
2025-03-01 07:43:52 +0000

    rs

--- core/powershell/storageAccount/rbac_assignment.ps1
+++ core/powershell/storageAccount/rbac_assignment.ps1
@@ -6,29 +6,13 @@ param (
 
 
 # Import CSV
-$rbacAssignments = Join-Path -Path $PSScriptRoot -ChildPath "rbac_assignment.csv" | Import-Csv 
+$rbacAssignments = Join-Path -Path $PSScriptRoot -ChildPath "../../../src/platform.operations/app/rbac_assignment.csv" | Import-Csv 
 
 # Loop through each row in the CSV
 foreach ($assignment in $rbacAssignments) {
     $container  = $assignment.Container
     $user       = $assignment.User
-
-    # Map roles
-    if ($Environment -eq "dev" -and $assignment.Role -eq "Super User"){
-        $role = " Advisory Custom Storage Container - Super User Dev"
-    }
-    elseif ($Environment -eq "dev" -and $assignment.Role -eq "Normal User"){
-        $role = " Advisory Custom Storage Container - Nomal Dev"
-    }
-    elseif ($Environment -eq "prod" -and $assignment.Role -eq "Super User"){
-        $role = "Advisory Custom Storage Container - Super Users"
-    }
-    elseif ($Environment -eq "prod" -and $assignment.Role -eq "Normal User"){
-        $role = "Advisory Custom Storage Container - Normal Users"
-    }
-    else{
-        $role = $assignment.Role
-    }
+    $role       = $assignment.Role
 
 
     # Get Storage Account resource
